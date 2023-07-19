06b65878b0621647e6a5dd081d8cf87aaec09484
User <mtgnatali@gmail.com>
2023-07-19 15:13:38 +0300

    add api with products

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,39 @@
-const Header = () => {
-    return (
-        <h2>Header</h2>
-    );
-}
+import { AppBar, Box, Toolbar, IconButton, Typography } from "@mui/material/";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Link } from "react-router-dom";
 
-export default Header;
+import HeaderThemeToggle from "./HeaderThemeToggle";
+import HeaderMenu from "./HeaderMenu";
+
+export default function Header(): JSX.Element {
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <Link to="/products" style={{ color: "inherit", textDecoration: "inherit" }}>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="open drawer"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+          </Link>
+          <HeaderThemeToggle />
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{ display: { xs: "none", sm: "block" } }}
+          >
+            Products website
+          </Typography>
+          <Box sx={{ flexGrow: 1 }} />
+          <HeaderMenu />
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
--- src/pages/ProductsPage.tsx
+++ src/pages/ProductsPage.tsx
@@ -4,17 +4,21 @@ import { Box, Stack } from "@mui/material";
 import { useAppDispatch } from "../hooks/reduxHooks";
 import { categoriesGet, productsGet } from "../api/productsWorker";
 import Footer from "../components/footer/Footer";
+import HomeBody from "../components/home/HomeBody";
+import Header from "../components/header/Header";
+import HomeAside from "../components/home/HomeAside";
+import HomeLinearProgress from "../components/home/HomeLinearProgress";
 
-const ProductsPage = () => {
+export default function ProductsPage() {
   const dispatch = useAppDispatch();
 
   useEffect(function () {
     dispatch(productsGet(""));
     dispatch(categoriesGet());
   }, []);
-  
-    return (
-        <Stack>
+
+  return (
+    <Stack>
       <Box
         margin="80px 0 90px 0"
         position="relative"
@@ -22,14 +26,28 @@ const ProductsPage = () => {
         minHeight="80vh"
         sx={{ backgroundColor: "background.default" }}
         display="flex"
-      >    
-        
-      </Box>     
+      >
+        <Box
+          width="15%"
+          position="fixed"
+          top="80"
+          padding="1rem 0"
+          bgcolor="secondary.main"
+          minHeight="78vh"
+        >
+          <HomeAside />
+        </Box>
+        <Box width="85%" marginLeft="15%">
+          <HomeBody />
+        </Box>
+      </Box>
+      <Box position="fixed" top="0" overflow="hidden" width="100%">
+        <Header />
+        <HomeLinearProgress />
+      </Box>
       <Box position="fixed" bottom="0" overflow="hidden" width="100%">
         <Footer />
       </Box>
     </Stack>
-    );
+  );
 }
-
-export default ProductsPage;
